6079a5ab57ed5284712a2084c7a17b17386e6d69
alex2718 <a.lee84@hotmail.com>
2022-03-27 18:37:08 +1100

    Add neural net unit tests

---
+++ tests/test_neuralnet.py
@@ -0,0 +1,11 @@
+from lexnet import __version__
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+def test_feedforward():
+    """
+    Test that the feedforward steps work for different
+    combinations of weights and architectures
+    """
+    assert True == True
